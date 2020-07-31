5e5eece4072d139d90d09a3719cb9ae2b1d216b5
vikasrajput <vikas_s_rajput@outlook.com>
2020-08-01 00:24:03 +1000

    added vmss steps

--- architect/300/vmss.sh
+++ architect/300/vmss.sh
@@ -3,20 +3,32 @@ datestr=`date +%Y%m%d`
 myResourceGroupLB="RG$datestr"
 
 #create rg
-az group create --name myResourceGroup --location eastus
+az group create --name $myResourceGroup --location eastus
 #create vmss 
 az vmss create \
-  --resource-group myResourceGroup \
+  --resource-group $myResourceGroup \
   --name myScaleSet \
   --image UbuntuLTS \
+  --vm-sku Standard_F1 \
+  --instance-count 3
   --admin-username azureuser \
   --generate-ssh-keys
 #create instance in vmss 
 az vmss list-instances \
-  --resource-group myResourceGroup \
+  --resource-group $myResourceGroup \
   --name myScaleSet \
   --output table
+#accessing instance details
 az vmss list-instance-connection-info \
-  --resource-group myResourceGroup \
+  --resource-group $myResourceGroup \
   --name myScaleSet
-  --instance-id 1
+az vmss show \
+    --resource-group $myResourceGroup \
+    --name myScaleSet \
+    --query [sku.capacity] \
+    --output table
+##managing instances at vmss level 
+#az vmss stop --resource-group $myResourceGroup --name myScaleSet --instance-ids 1
+#az vmss deallocate --resource-group $myResourceGroup --name myScaleSet --instance-ids 1
+#az vmss start --resource-group $myResourceGroup --name myScaleSet --instance-ids 1
+#az vmss restart --resource-group myResourceGroup --name myScaleSet --instance-ids 1
